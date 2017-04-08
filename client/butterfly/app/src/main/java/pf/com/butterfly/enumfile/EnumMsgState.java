d61a6249c830336af4c4e32c44690ce8385788c4
pingfan121 <956184289@qq.com>
2017-04-09 03:44:48 +0800

    //修改了协议生成

---
+++ client/butterfly/app/src/main/java/pf/com/butterfly/enumfile/EnumMsgState.java
@@ -0,0 +1,15 @@
+package pf.com.butterfly.enumfile;
+
+public class EnumMsgState
+{
+   public static final int ok = 0; //没有错误
+   public static final int error = 1; //通用错误
+   public static final int sign_err = 100; //签名错误(100-200)属于通信错误
+   public static final int pro_analysis_err = 101; //协议解析错误
+   public static final int token_past = 200; //token过期了
+   public static final int err_shuoming = 1000; //1000之后为协议处理错误
+   public static final int reg_mob_exist = 1001; //手机号已经存在
+   public static final int reg_mob_format_err = 1002; //手机号格式不正确
+   public static final int login_password_err = 1003; //密码错误
+   public static final int login_mob_not_reg = 1004; //手机号没有注册
+}
